a0243ace2b4b4f651d6b56a385bc1dd706ed75e0
Josh Lehr <joshua.valian17@gmail.com>
2024-03-31 18:42:06 -0600

    Add delete post function

--- index.js
+++ index.js
@@ -10,26 +10,34 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: posts});
 });
 
 app.post("/create-post", (req, res) => {
     const newPost = {
+        id: posts.length + 1,
         title: req.body["title"],
         body: req.body["body"],
     }
     posts.push(newPost);
-    res.render("index.ejs", {posts: posts});
+    res.redirect("/");
 });
 
 app.put("/edit-post", (req, res) => {
+    newTitle = req.body.editTitle;
+    newBody = req.body.editBody;
+    console.log(newTitle);
+    console.log(newBody);
     // Add new post to posts. Pass posts as a variable for ejs to loop through.
-    res.render("index.ejs");
+    res.redirect("/");
 });
 
-app.delete("/edit-post", (req, res) => {
+app.post("/delete-post", (req, res) => {
+    const postToDelete = req.body.id;
+    console.log(postToDelete);
+    posts.pop(req.body['postToDelete']);
     // Add new post to posts. Pass posts as a variable for ejs to loop through.
-    res.render("index.ejs");
+    res.redirect("/");
 });
 
 app.listen(port, () => {
